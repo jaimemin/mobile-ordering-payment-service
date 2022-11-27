c19c51c0308c98c4f19dfb4ce978fe0b994aa59f
jaimemin <jaimemin@gmail.com>
2022-11-28 01:10:28 +0900

    implemented header and toggle side bar

--- src/main/java/com/tistory/amyyzzin/mobileorderingpaymentservice/controller/WebController.java
+++ src/main/java/com/tistory/amyyzzin/mobileorderingpaymentservice/controller/WebController.java
@@ -4,6 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class WebController {
 
+    /**
+     * sidebar toggle 참고: https://codesandbox.io/s/fb4qc?file=/index.html:0-7911
+     *
+     * @return
+     */
     @GetMapping("/")
     public String index() {
         return "index";
