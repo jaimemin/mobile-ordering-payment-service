d3cc66f759d1d75dd3910098b44a2c377b508aa2
jaimemin <jaimemin@gmail.com>
2022-11-27 17:36:00 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "mobile-ordering-payment-service"
---
+++ src/main/java/com/tistory/amyyzzin/mobileorderingpaymentservice/MobileOrderingPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tistory.amyyzzin.mobileorderingpaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileOrderingPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobileOrderingPaymentServiceApplication.class, args);
+    }
+
+}
